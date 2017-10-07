eb06e996ead8b0d29bafe4748a207e54bffe247d
Tania El-Asmar <Tania@Tanias-MBP.local>
2017-10-07 15:10:03 -0400

    modification entities Login

--- src/enitities/Login.java
+++ src/enitities/Login.java
@@ -1,6 +1,11 @@
 package enitities;
 
 public class Login {
+	private int id;
+	private String nom;
+	private String prenom;
+	private String phone;
+	private String adresse;
 	private String username;
 	private String password;
 	
@@ -11,6 +16,46 @@ public class Login {
 	public Login(String username,String password){
 		this.username=username;
 		this.password=password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
 	}
 
 	public String getUsername() {
@@ -28,6 +73,4 @@ public class Login {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
 }
